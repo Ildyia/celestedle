737740f6305f3653dba63697967b32282b8e8d5f
Ildyia <amelia.matarese.perso@gmail.com>
2026-08-06 17:32:18 +0200

    maybe its a fix maybe not

--- src/server.js
+++ src/server.js
@@ -22,6 +22,10 @@ app.use("/", gameRoutes);
 app.use("/admin", adminRoutes);
 app.use("/report-bug", reportRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server successfully started running on port ${PORT}`);
