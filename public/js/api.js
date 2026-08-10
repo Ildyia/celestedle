c91dbf9961dc50186da22ee5c51238b139d7c604
Ildyia <amelia.matarese.perso@gmail.com>
2026-08-10 18:40:40 +0200

    export api adress

--- public/js/api.js
+++ public/js/api.js
@@ -1,6 +1,6 @@
 const hostname = window.location.hostname;
 
-const API_BASE_URL =
+export const API_BASE_URL =
   hostname === "localhost" ||
   hostname === "127.0.0.1" ||
   hostname === "::1" ||
