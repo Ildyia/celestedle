ecd377e07b3a665c02eb5144a15cc78baaf7bae7
Ildyia <amelia.matarese.perso@gmail.com>
2026-09-15 17:20:31 +0200

    bruh

--- src/server.js
+++ src/server.js
@@ -33,7 +33,6 @@ app.use(
     credentials: true
   })
 );
-app.use("/report", reportRoutes);
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
